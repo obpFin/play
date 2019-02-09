9c38d908769e3c8a0cc429769821e9232d9213c9
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-02-09 22:22:06 +0200

    demo controller

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
+var bodyParser = require("body-parser");
 var core_1 = require("@overnightjs/core");
 var simple_color_print_1 = require("simple-color-print");
+var PlayController_1 = require("./controllers/PlayController");
 var PlayServer = (function (_super) {
     tslib_1.__extends(PlayServer, _super);
     function PlayServer() {
         var _this = _super.call(this) || this;
-        _this._port = 3001;
         _this._SERVER_START_MSG = 'Demo server started on port: ';
+        _this._DEV_MSG = 'Express Server is running in development mode. Start the React ' +
+            'development server "npm run start:react" to develop front-end content. Back-end is ' +
+            'currently running on port: ';
+        _this._port = 3001;
+        _this.app.use(bodyParser.json());
+        _this.app.use(bodyParser.urlencoded({ extended: true }));
+        _super.prototype.addControllers.call(_this, new PlayController_1.default());
+        if (process.env.NODE_ENV === 'development') {
+            simple_color_print_1.cinfo('Starting server in development mode');
+            var msg_1 = _this._DEV_MSG + _this._port;
+            _this.app.get('*', function (req, res) { return res.send(msg_1); });
+        }
         return _this;
     }
     PlayServer.prototype.start = function () {
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,48 @@
+import * as bodyParser from 'body-parser';
 import { Server } from '@overnightjs/core';
-import { cimp } from 'simple-color-print';
+import { cimp, cinfo } from 'simple-color-print';
+import PlayController from './controllers/PlayController';
+
 
 class PlayServer extends Server {
-  private _port = 3001;
-  private readonly _SERVER_START_MSG = 'Server started on port: ';
 
-  constructor() {
-    super();
-  }
+    private readonly _SERVER_START_MSG = 'Demo server started on port: ';
+    private readonly _DEV_MSG = 'Express Server is running in development mode. Start the React ' +
+        'development server "npm run start:react" to develop front-end content. Back-end is ' +
+        'currently running on port: ';
 
-  public start(): void {
-    this.app.listen(this._port, () => {
-      cimp(this._SERVER_START_MSG + this._port);
-    });
-  }
+    private _port = 3001;
+
+
+    constructor() {
+        super();
+
+        // Setup json middleware
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: true}));
+
+        // Setup the controllers
+        super.addControllers(new PlayController());
+
+        // Point to front-end code
+        if (process.env.NODE_ENV === 'development') {
+            cinfo('Starting server in development mode');
+            const msg = this._DEV_MSG + this._port;
+            this.app.get('*', (req, res) => res.send(msg));
+        }
+    }
+
+    public start(): void {
+        this.app.listen(this._port, () => {
+            cimp(this._SERVER_START_MSG + this._port);
+        });
+    }
 }
 
 /**
  * Start the server
  */
 (() => {
-  const server = new PlayServer();
-  server.start();
+    const server = new PlayServer();
+    server.start();
 })();
