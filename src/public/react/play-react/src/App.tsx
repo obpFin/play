2c02adba8fdb667a16240c299aa2c1cd36eb4a2d
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-02-14 20:46:34 +0200

    cleanup

--- src/public/react/play-react/src/App.tsx
+++ src/public/react/play-react/src/App.tsx
@@ -5,19 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     async function callExpress() {
-
       try {
-
-        let response = await fetch('/api/say-hello/SeanMaxwell')
+        let response = await fetch('/api/say-hello/OskariPeltonen')
                               .then(res => res.json());
-
         alert('Hi this is a response from the backend: ' + response.response);
-
       } catch (err) {
         alert(err);
       }
     }
-
     callExpress();
     return (
       <div className="App">
