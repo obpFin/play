c64bb5e9ea1f8e1c13a5baf5558ec9bc621e4299
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-02-20 20:30:18 +0200

    another example

--- src/public/react/play-react/src/components/Hooks/Hooks.tsx
+++ src/public/react/play-react/src/components/Hooks/Hooks.tsx
@@ -1,13 +1,43 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 
 export const Counter = () => {
+
+  const fetchQuote = () => {
+    fetch('http://api.icndb.com/jokes/random')
+    .then(res => res.json())
+    .then(data => {
+      if (data.value) {
+        setQuote(data.value.joke)
+      } else (
+        setQuote('Unable to fetch quote')
+      )
+    })
+  }
+
   const [count, setCount] = React.useState(0)
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  const [quote, setQuote] = React.useState('')
+  useEffect(() => {
+    fetchQuote()
+  }, []);
 
   return (
-    <div className="counter">
-      <h2>React Hooks example</h2>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count+1)}>Click me</button>
+    <div>
+      <h1>React Hooks examples</h1>
+      <div className="item">
+        <h2>State within functional component</h2>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count+1)}>Click me</button>
+      </div>
+      <div className="item">
+        <h2>Side-effects without lifecycle methods</h2>
+        <strong><p>Random Chuck Norris quote</p></strong>
+        <p>{quote}</p>
+        <button onClick={fetchQuote}>New quote</button>
+      </div>
     </div>
   )
 }
