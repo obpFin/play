a51f2cb2dd8db2ff9ccf380c1b67669b622fdecf
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-06-28 13:02:56 +0300

    add block scoping example

--- with-js-closures/excercises.js
+++ with-js-closures/excercises.js
@@ -1,29 +1,39 @@
 // Make it so that the initialize function can only be called once!
-// let view;
-// function initialize() {
-//   let called = 0;
-//   return function() {
-//     if (called > 0) {
-//       return;
-//     } else {
-//       called++;
-//       view = '🏔';
-//       console.log('view has been set!');
-//     }
-//   }
-// }
+let view;
+function initialize() {
+  let called = 0;
+  return function () {
+    if (called > 0) {
+      return;
+    } else {
+      called++;
+      view = '🏔';
+      console.log('view has been set!');
+    }
+  };
+}
 
-// const startOnce = initialize();
-// startOnce();
-// startOnce();
+const startOnce = initialize();
+startOnce();
+startOnce();
 
-// console.log(view);
+console.log(view);
 
 const array = [1, 2, 3, 4];
+console.log('With iife')
 for (var i = 0; i < array.length; i++) {
   (function (closureI) {
     setTimeout(function () {
       console.log('I am at index ' + array[closureI]);
     }, 3000);
   })(i);
+}
+
+// OR
+const array2 = [1, 2, 3, 4];
+console.log('With block scoping')
+for (let i = 0; i < array2.length; i++) {
+  setTimeout(function () {
+    console.log('I am at index ' + array2[i]);
+  }, 3000);
 }
