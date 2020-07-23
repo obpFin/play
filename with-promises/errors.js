41a1df8da5af3e6fcefba72b95df46f311f2a030
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-07-23 13:43:41 +0300

    error handling with promises

---
+++ with-promises/errors.js
@@ -0,0 +1,21 @@
+// asynchronous error handling
+
+Promise.resolve('asyncfail')
+    .then(response => {
+        console.log(response)
+        throw new Error('#1 fail')
+    })
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => {
+        console.error('error', err.message)
+    })
+    .then(response => {
+        console.log('hi am I still needed?', response)
+        return 'done'
+    })
+    .catch(err => {
+        console.error(err)
+        return 'failed'
+    })
